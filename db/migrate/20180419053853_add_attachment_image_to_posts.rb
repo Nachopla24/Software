b4451facdc5f7511aa0e2d7a7068477522b981c2
Jotaka <jagc1991@gmail.com>
2018-04-23 23:19:48 -0300

    Eliminacion de un 'end' en la migracion image_to_posts

--- db/migrate/20180419053853_add_attachment_image_to_posts.rb
+++ db/migrate/20180419053853_add_attachment_image_to_posts.rb
@@ -2,7 +2,6 @@ class AddAttachmentImageToPosts < ActiveRecord::Migration[4.2]
     change_table :posts do |t|
       t.attachment :image
     end
-  end
 
   def self.down
     remove_attachment :posts, :image
