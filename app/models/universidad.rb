948f189d5946319e22595b79f0810a630e54933f
Nachopla24 <ignacio.arayal@utem.cl>
2018-05-03 19:25:11 -0300

    Se elimina tabla departamentos y se crea una llave foranea en carreras de universidad, y ademas se valida universidad

--- app/models/universidad.rb
+++ app/models/universidad.rb
@@ -2,4 +2,6 @@ class Universidad < ApplicationRecord
 	has_many :carreras
 	validates :nombre, presence: {:message => 'No puede quedar vacío'}
 	validates :nombre, length: {in: 7..60, too_short: "Demasiado corto", too_long: "Demasiado largo"}
+	validates :descripcion, presence: {:message => 'No puede quedar vacío'}
+	validates :descripcion, length: { minimum: 10 }
 end
