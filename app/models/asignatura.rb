0cef9f60a141650bae5e9ef72b407ba2cd814548
Jotaka <jagc1991@gmail.com>
2018-05-03 19:34:42 -0300

    Validacion del largo de la descripcion de asignatura y carrera

--- app/models/asignatura.rb
+++ app/models/asignatura.rb
@@ -3,5 +3,5 @@ class Asignatura < ApplicationRecord
 	has_and_belongs_to_many :alumnos, dependent: :destroy
 	validates :nombre, presence: {:message => 'No puede quedar vacío'}
 	validates :nombre, length: {in: 7..60, too_short: "Demasiado corto", too_long: "Demasiado largo"}
-	
+	validates :descripcion, length: {maximum: 500, too_long: ": %{count} caracteres es el máximo permitido"}
 end
--- app/models/carrera.rb
+++ app/models/carrera.rb
@@ -3,5 +3,5 @@ class Carrera < ApplicationRecord
 	belongs_to :departamento
 	validates :nombre, presence: {:message => 'No puede quedar vacío'}
 	validates :nombre, length: {in: 7..80, too_short: "Demasiado corto", too_long: "Demasiado largo"}
-	
+	validates :descripcion, length: {maximum: 500, too_long: ": %{count} caracteres es el máximo permitido"}
 end
