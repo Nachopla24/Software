eb61988e53128dbffef7f644e2ebf179802ad635
Gerulps25 <Geraldine.93@outlook.com>
2018-05-04 03:04:05 -0300

    Eliminacion de 3 migraciones drop (drop_tablealumno_asignatura, drop_tablecarrera_asignatura, drop_table_post)

--- db/migrate/20180430181722_drop_tablealumno_asignaturas.rb
+++
@@ -1,5 +0,0 @@
-class DropTablealumnoAsignaturas < ActiveRecord::Migration[5.1]
-  def change
-  	drop_table :alumno_asignaturas
-  end
-end
--- db/migrate/20180430181747_drop_tablecarrera_asignaturas.rb
+++
@@ -1,5 +0,0 @@
-class DropTablecarreraAsignaturas < ActiveRecord::Migration[5.1]
-  def change
-  	drop_table :carrera_asignaturas
-  end
-end
