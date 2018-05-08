4b065e86f09eaf43829d8bfc07f6c53c40fb5233
Jotaka <jagc1991@gmail.com>
2018-05-08 04:39:02 -0300

    Cambio en la migracion drop_table_departamentos, se borro el atributo departamento_id de universidads para poder eliminar la tabla departamentos

--- db/migrate/20180503214845_drop_table_departamentos.rb
+++ db/migrate/20180503214845_drop_table_departamentos.rb
@@ -1,6 +1,6 @@
 class DropTableDepartamentos < ActiveRecord::Migration[5.1]
   def change
-  	remove_column :carreras, :departamento_id, :integer
+  	remove_column :universidads, :departamento_id, :integer
   	drop_table :departamentos
 
   end
