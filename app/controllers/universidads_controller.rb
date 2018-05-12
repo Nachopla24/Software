92d396b2b4335073d573456712d405ad49bb5138
Jotaka <jagc1991@gmail.com>
2018-05-12 04:49:41 -0300

    Validacion en universidad, se valido el formato del nombre y se tradujeron algunas palabras en las vistas

--- app/controllers/universidads_controller.rb
+++ app/controllers/universidads_controller.rb
@@ -28,7 +28,7 @@ class UniversidadsController < ApplicationController
 
     respond_to do |format|
       if @universidad.save
-        format.html { redirect_to @universidad, notice: 'Universidad was successfully created.' }
+        format.html { redirect_to @universidad, notice: 'La universidad fue creada con éxito.' }
         format.json { render :show, status: :created, location: @universidad }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class UniversidadsController < ApplicationController
   def update
     respond_to do |format|
       if @universidad.update(universidad_params)
-        format.html { redirect_to @universidad, notice: 'Universidad was successfully updated.' }
+        format.html { redirect_to @universidad, notice: 'La universidad fue actualizada con éxito.' }
         format.json { render :show, status: :ok, location: @universidad }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class UniversidadsController < ApplicationController
   def destroy
     @universidad.destroy
     respond_to do |format|
-      format.html { redirect_to universidads_url, notice: 'Universidad was successfully destroyed.' }
+      format.html { redirect_to universidads_url, notice: 'La universidad fue eliminada con éxito.' }
       format.json { head :no_content }
     end
   end
--- app/models/universidad.rb
+++ app/models/universidad.rb
@@ -1,6 +1,7 @@
 class Universidad < ApplicationRecord
 	has_many :carreras
 	validates :nombre, presence: true
-	validates :nombre, length: {in: 7..60, too_short: "Demasiado corto", too_long: "Demasiado largo"}
+	validates :nombre, length: {in: 15..60}
+	validates_format_of :nombre, :with => /\A[A-Z a-z á é í ó ú ñ - ']+\z/
 	validates :descripcion, presence: true
 end
