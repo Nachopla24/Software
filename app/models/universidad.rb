d9b12a3987a89d40ab2a6aad8cf30099b9bd094d
Jotaka <jagc1991@gmail.com>
2018-05-12 23:03:47 -0300

    Validacion en universidad, se valido el formato de direccion, el largo de direccion, nombre, numero de direccion y descripcion, y se valido numeros positivos en el numero de direccion

--- app/models/universidad.rb
+++ app/models/universidad.rb
@@ -3,5 +3,12 @@ class Universidad < ApplicationRecord
 	validates :nombre, presence: true
 	validates :nombre, length: {in: 15..60}
 	validates_format_of :nombre, :with => /\A[A-Z a-z á é í ó ú ñ - ']+\z/
+	validates :direccion, presence: true
+	validates :direccion, length: {in: 7..60}
+	validates_format_of :direccion, :with => /\A[A-Z a-z á é í ó ú ñ - ' ü]+\z/
+	validates :numDireccion, presence: true
+	validates :numDireccion, length: {in: 2..5}
+	validates :numDireccion, numericality: {greater_than: 0}
 	validates :descripcion, presence: true
+	validates :descripcion, length: {in: 3..500}
 end
