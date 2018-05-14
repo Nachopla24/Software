b09d831c41327f680bd96e9a48e3cd0ecf83b7a8
Gerulps25 <Geraldine.93@outlook.com>
2018-05-14 19:29:21 -0400

    Borrar un error al crear nueva carrera y validación de Nombre de carrera

--- app/models/carrera.rb
+++ app/models/carrera.rb
@@ -1,8 +1,8 @@
 class Carrera < ApplicationRecord
 	belongs_to :universidad
 	has_many :carreraAsignatura	
-	belongs_to :departamento
 	validates :nombre, presence: true
 	validates :nombre, length: {in: 7..80, too_short: "Demasiado corto", too_long: "Demasiado largo"}
+	validates_format_of :nombre, :with => /\A[A-Z a-z á é í ó ú ñ - ']+\z/
 	validates :descripcion, length: {maximum: 500, too_long: ": %{count} caracteres es el máximo permitido"}
 end
