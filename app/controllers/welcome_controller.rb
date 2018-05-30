02532571189a967f9a2039463777ea29923e4993
Gerulps25 <Geraldine.93@outlook.com>
2018-05-30 03:52:51 -0400

    Ordenamiento de posts recientes de un usuario logeado

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
   	@posts = Post.all.order(id: :desc)
+  	if user_signed_in?
+  	@posts = Post.order(id: :desc)
+  	             .where user_id: current_user.id
+  	end
   end
 end
