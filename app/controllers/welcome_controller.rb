af947aed67451242eedfb1d193c06ff88c6ff0fd
Gerulps25 <Geraldine.93@outlook.com>
2018-05-30 19:50:34 -0400

    Paginacion publicaciones del home

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,7 @@
 class WelcomeController < ApplicationController
   def index
-  	@posts = Post.all.order(id: :desc)
-  	if user_signed_in?
-  	@posts = Post.order(id: :desc)
-  	             .where user_id: current_user.id
-  	end
+  	@posts = Post.all.paginate(page: params[:page], per_page: 6)
+  					.order(id: :desc)
+  	
   end
 end
