a81d9594a692892469249f020a098b24cfad4748
Gerulps25 <Geraldine.93@outlook.com>
2018-06-22 02:29:40 -0400

    Boton de Likes de los posts creado y contando con diferentes usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
   resources :asignaturas
   resources :carreras
   resources :universidads
   root 'welcome#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts do 
+    member do 
+      put "like" => "posts#upvote"
+    end
+  end
 end
+
+  
